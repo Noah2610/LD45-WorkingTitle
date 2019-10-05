57e161b4b93103e804740c3ab88f9071a57ac13c
hoichael <emurenn@gmail.com>
2019-10-05 15:00:09 +0200

    Add spritesheet dev_solid1, update tsx

--- maps/solids_concept.tsx
+++ maps/solids_concept.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="solids_concept" tilewidth="16" tileheight="16" tilecount="2" columns="2">
- <image source="../png/conceptshizzle.png" width="32" height="16"/>
+ <image source="../resources/spritesheets/dev_solid1.png" width="32" height="16"/>
 </tileset>
