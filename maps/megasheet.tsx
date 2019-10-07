81d5951c0d4ff97e24855a7ee18f25b077bc7d51
Emu <emurenn@gmail.com>
2019-10-07 10:57:22 +0200

    tmx, tsx, json, png, ron

---
+++ maps/megasheet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="megasheet" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../resources/spritesheets/megasheet.png" width="256" height="256"/>
+ <tile id="9">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
