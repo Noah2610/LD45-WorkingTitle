4cdd5c17dc7d45109312a6c0e82d003090fa1f5c
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-07 22:41:31 +0200

    Fix try_run bug

--- bin/travis-share.sh
+++ bin/travis-share.sh
@@ -33,7 +33,6 @@ function try_run {
   [ -z "$cmd" ] && err "No command given."
   local out
   local out_files=("$LOGFILE")
-  [ -n "$also_to_stderr" ] && out_files+=("/dev/stderr")
   msg "${spacing}Running: \033[${COLOR_CODE}m${cmd}\033[m"
   if ! $cmd 2>&1 | tee -a "${out_files[@]}"; then
     err "Command failed:\n  \033[${COLOR_CODE}m${cmd}\033[m\nReturned:\n${out}"
