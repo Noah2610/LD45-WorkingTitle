e54c8ad88c8b41bb0ef0c89e9bd977ce6275c5e0
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-07 15:55:37 +0200

    Replace '/' with '\' in helpers::resource for windows target

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,9 +3,15 @@ where
     S: ToString,
 {
     use amethyst::utils::app_root_dir::application_dir;
+    let path = if cfg!(target = "windows") {
+        path.to_string().replace("/", "\\")
+    } else {
+        path.to_string()
+    };
+
     let res_dir =
         application_dir("resources").expect("Should have resources directory");
 
-    let path = res_dir.join(path.to_string());
+    let path = res_dir.join(path);
     path.to_str().unwrap().to_string()
 }
