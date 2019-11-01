b31029f02e192fc3edccf204e269765c28bb942c
Emu <emurenn@gmail.com>
2019-11-01 21:07:41 +0100

    minor adjustments

--- maps/plants.tsx
+++ maps/plants.tsx
@@ -1,4 +1,100 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="plants" tilewidth="16" tileheight="16" tilecount="128" columns="16">
  <image source="../resources/spritesheets/plants.png" width="256" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="2, 4, 6, 4, 2"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="3, 5, 7, 5, 3"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="0, 4, 6, 4, 0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="1, 5, 7, 5, 1"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="0, 2, 6, 2, 0"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="1, 3, 7, 3, 1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="0, 2, 4, 2, 0"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="1, 3, 5, 3, 1"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="18, 20, 22, 20, 18"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="19, 21, 23, 21, 19"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="16, 20, 22, 20, 16"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="17, 21, 23, 21, 17"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="16, 18, 22, 18, 16"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="17, 19, 23, 19, 17"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="16, 18, 20, 18, 16"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="animation_delays_ms" value="200, 200, 200, 200, 200"/>
+   <property name="animation_sprite_ids" value="17, 19, 21, 19, 17"/>
+  </properties>
+ </tile>
 </tileset>
