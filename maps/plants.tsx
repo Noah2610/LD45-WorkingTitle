9fe95e5c23f60e213fb8986a370c8bac1ea48059
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-11-02 23:37:07 +0100

    Level tweaks and cleanup

--- maps/plants.tsx
+++ maps/plants.tsx
@@ -3,30 +3,35 @@
  <image source="../resources/spritesheets/plants.png" width="256" height="128"/>
  <tile id="0">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="0, 2, 4, 6, 4, 2"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="1, 3, 5, 7, 5, 3"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="16, 18, 20, 22, 20, 18"/>
   </properties>
  </tile>
  <tile id="17">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="17, 19, 21, 23, 21, 19"/>
   </properties>
  </tile>
  <tile id="34">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="200, 200, 200, 200"/>
    <property name="animation_sprite_ids" value="34, 37, 10, 66"/>
   </properties>
@@ -39,42 +44,49 @@
  </tile>
  <tile id="40">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="40, 42, 44, 92, 94, 92, 44, 42"/>
   </properties>
  </tile>
  <tile id="41">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="41, 43, 45, 93, 95, 93, 45, 43"/>
   </properties>
  </tile>
  <tile id="48">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="200, 200, 200, 20"/>
    <property name="animation_sprite_ids" value="48, 51, 24, 80"/>
   </properties>
  </tile>
  <tile id="49">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="200, 200, 200, 200"/>
    <property name="animation_sprite_ids" value="49, 52, 25, 81"/>
   </properties>
  </tile>
  <tile id="50">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="200, 200, 200, 200"/>
    <property name="animation_sprite_ids" value="50, 53, 26, 82"/>
   </properties>
  </tile>
  <tile id="56">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="56, 58, 60, 108, 110, 108, 60, 58"/>
   </properties>
  </tile>
  <tile id="57">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="57, 59, 51, 109, 111, 109, 61, 59"/>
   </properties>
@@ -87,12 +99,14 @@
  </tile>
  <tile id="72">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="72, 74, 76, 124, 126, 124, 76, 74"/>
   </properties>
  </tile>
  <tile id="73">
   <properties>
+   <property name="always_loaded" type="bool" value="true"/>
    <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150, 150"/>
    <property name="animation_sprite_ids" value="73, 75, 77, 125, 127, 125, 77, 75"/>
   </properties>
