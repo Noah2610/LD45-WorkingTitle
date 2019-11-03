ce490c29b014dc2b770d744c63713b52895dbf49
Emu <emurenn@gmail.com>
2019-11-03 22:04:30 +0100

    New spritesheet (png, tsx, ron) + level 1 changes

---
+++ maps/shroomsheet.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="shroomsheet" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+ <image source="../resources/spritesheets/shroomsheet.png" width="144" height="144"/>
+ <tile id="0">
+  <properties>
+   <property name="always_loaded" type="bool" value="true"/>
+   <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150"/>
+   <property name="animation_sprite_ids" value="0, 3, 6, 18, 21, 24, 36"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="always_loaded" type="bool" value="true"/>
+   <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150"/>
+   <property name="animation_sprite_ids" value="1, 4, 7, 19, 22, 25, 37"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="always_loaded" type="bool" value="true"/>
+   <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150"/>
+   <property name="animation_sprite_ids" value="9, 12, 15, 27, 30, 33, 45"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="always_loaded" type="bool" value="true"/>
+   <property name="animation_delays_ms" value="150, 150, 150, 150, 150, 150, 150"/>
+   <property name="animation_sprite_ids" value="10, 13, 16, 28, 31, 34, 46"/>
+  </properties>
+ </tile>
+</tileset>
