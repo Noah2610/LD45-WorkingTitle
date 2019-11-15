9c80e683f952dd39511d19069cc886447e37f6bf
Emu <emurenn@gmail.com>
2019-11-15 22:39:44 +0100

    Grass animation prep, incomplete spritesheet
    Routine commit

---
+++ maps/grass.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="grass" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="../resources/spritesheets/grass.png" width="128" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="dynamic_animation" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="dynamic_animation" value=""/>
+  </properties>
+ </tile>
+</tileset>
