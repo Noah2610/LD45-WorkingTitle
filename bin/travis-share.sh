eeae6ec818ca410fa23f351356c41956a98020ad
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-11-16 14:43:32 +0100

    Add telegram bot /sendMessage calls to travis pipeline

--- bin/travis-share.sh
+++ bin/travis-share.sh
@@ -10,17 +10,17 @@ unset _dir
 [ -z "$TRAVIS" ] && err "This script should only be run from Travis"
 
 function get_release_path {
-  check "cat"
-  local file_with_path="${ROOT}/.travis-release-path-${TARGET}"
-  check_file "$file_with_path"
-  cat "$file_with_path"
+    check "cat"
+    local file_with_path="${ROOT}/.travis-release-path-${TARGET}"
+    check_file "$file_with_path"
+    cat "$file_with_path"
 }
 
 function pushd_wrapper {
-  \pushd "$@" &> /dev/null || exit 1
+    \pushd "$@" &> /dev/null || exit 1
 }
 function popd_wrapper {
-  \popd "$@" &> /dev/null || exit 1
+    \popd "$@" &> /dev/null || exit 1
 }
 
 # Copied from util.sh
@@ -29,14 +29,29 @@ function popd_wrapper {
 # If the variable `$also_to_stderr` is set, then additionally to writing
 # the commands output to the `$LOGFILE`, it also prints it to stderr.
 function try_run {
-  local cmd="$1"
-  [ -z "$cmd" ] && err "No command given."
-  local out
-  local out_files=("$LOGFILE")
-  msg "${spacing}Running: \033[${COLOR_CODE}m${cmd}\033[m"
-  if ! $cmd 2>&1 | tee -a "${out_files[@]}"; then
-    err "Command failed:\n  \033[${COLOR_CODE}m${cmd}\033[m\nReturned:\n${out}"
-  fi
+    local cmd="$1"
+    [ -z "$cmd" ] && err "No command given."
+    local out
+    local out_files=("$LOGFILE")
+    msg "${spacing}Running: \033[${COLOR_CODE}m${cmd}\033[m"
+    if ! $cmd 2>&1 | tee -a "${out_files[@]}"; then
+        err "Command failed:\n  \033[${COLOR_CODE}m${cmd}\033[m\nReturned:\n${out}"
+    fi
+}
+
+function send_telegram_message {
+    { [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT_ID" ];  } \
+        && return 0
+
+    local message="$1"
+    [ -z "$message" ] && err "Function \`send_telegram_message\` requires one argument as the message to send"
+
+    local url="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+    curl -s -X POST "$url" \
+        -d chat_id="$TELEGRAM_CHAT_ID" \
+        -d text="$message" \
+        -d parse_mode="Markdown"
 }
 
 alias pushd="pushd_wrapper"
