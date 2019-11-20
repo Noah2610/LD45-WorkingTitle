602dfb7b593beb0d007f630bed2221790ec87f14
Emu <emurenn@gmail.com>
2019-11-20 07:42:26 +0100

    New level, additional dynamic animations, general changes to levels 1-3 (minor)

--- maps/grass.tsx
+++ maps/grass.tsx
@@ -1,6 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="grass" tilewidth="16" tileheight="16" tilecount="32" columns="8">
  <image source="../resources/spritesheets/grass.png" width="128" height="64"/>
+ <tile id="7">
+  <properties>
+   <property name="dynamic_animation" value="dyn_purplebg1.ron"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="dynamic_animation" value="dyn_purplefg1.ron"/>
+  </properties>
+ </tile>
  <tile id="18">
   <properties>
    <property name="dynamic_animation" value="dyn_redbg1.ron"/>
