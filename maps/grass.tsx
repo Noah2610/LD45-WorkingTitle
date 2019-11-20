2b0c602154a91fc994de19e4adaa493077f2c33f
Emu <emurenn@gmail.com>
2019-11-20 02:54:20 +0100

    Dynamic grass animations update (rons, png, tmx, json)

--- maps/grass.tsx
+++ maps/grass.tsx
@@ -1,19 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="grass" tilewidth="16" tileheight="16" tilecount="32" columns="8">
  <image source="../resources/spritesheets/grass.png" width="128" height="64"/>
- <tile id="0">
+ <tile id="18">
   <properties>
-   <property name="dynamic_animation" value="dyn_redgrass1.ron"/>
+   <property name="dynamic_animation" value="dyn_redbg1.ron"/>
   </properties>
  </tile>
- <tile id="3">
+ <tile id="19">
   <properties>
-   <property name="dynamic_animation" value="dyn_redgrass2.ron"/>
+   <property name="dynamic_animation" value="dyn_redbg2.ron"/>
   </properties>
  </tile>
- <tile id="16">
+ <tile id="29">
   <properties>
-   <property name="dynamic_animation" value="dyn_purplegrass1.ron"/>
+   <property name="dynamic_animation" value="dyn_redfg1.ron"/>
   </properties>
  </tile>
 </tileset>
